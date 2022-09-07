2780bf6c651e692e31195b955b2c3360a664718a
Itoo <74044271+ItooItoo@users.noreply.github.com>
2022-09-07 09:19:53 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-api = '080f0ae474b49f6788a765043df219a2'
-
-weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Ołpiny&APPID={api}")
-
-print(weather.json()['wind']['speed'])
